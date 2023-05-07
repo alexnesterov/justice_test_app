1c0094809544739cc11d2c59372764950ba1ce03
Alex Nesterov <ialexnesterov@gmail.com>
2023-05-07 11:55:13 +0300

    Инициализирован репозиторий с начальной настройкой

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ProductFilter } from '@/components'
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto px-6">
+        <ProductFilter />
+      </div>
+    </>
+  )
+}
+
+export default App
